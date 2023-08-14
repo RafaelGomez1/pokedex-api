b6d570e58eef556fec91f22ba7b6a06cad00f908
rgomez <rafael.gomez@code.seat>
2023-08-14 19:01:38 +0200

    ADD JIB to build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
+    id("com.google.cloud.tools.jib") version "3.1.4"
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.spring") version "1.9.0"
     id("java-test-fixtures")
